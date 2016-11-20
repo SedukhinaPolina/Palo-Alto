528373d3afde9365ae12df1ef7261c5a4e234ac8
Polina Sedukhina <sedukhina.polina@gmail.com>
2016-11-20 21:47:23 +0600

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var nunjucks = require('gulp-nunjucks');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+var cache = require('gulp-cache');
+var livereload = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
+var path = {
+    css:  'src/styles/*.css',
+    html: 'src/templates/*.html',
+    images: 'src/images/*.*',
+    dist: {
+      css:  'dist/styles/',
+      html: 'dist/',
+      images: 'dist/images/'
+    }
+};
+
+
+gulp.task('default', ['build', 'serve', 'watch']);
+
+gulp.task('css', function () {
+  return gulp.src(path.css)
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+gulp.task('html', function () {
+  return gulp.src(path.html)
+    .pipe(nunjucks.compile())
+    .pipe(gulp.dest(path.dist.html));
+});
+
+gulp.task('images', function() {
+  return gulp.src(path.images)
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(livereload(server))
+    .pipe(gulp.dest(path.dist.images));
+});
+
+
+gulp.task('build', ['html', 'css', 'images']);
+
+gulp.task('watch', function () {
+  gulp.watch(path.css, ['css']);
+  gulp.watch(path.html, ['html']);
+  gulp.watch(path.images, ['images']);
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: path.dist.html
+    }
+  });
+  gulp.watch('dist/*').on('change', browserSync.reload);
+});
